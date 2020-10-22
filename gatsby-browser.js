e233d650610384338fea3c4a2b22919758d0d983
Lunesa <gmlunesa@gmail.com>
2020-10-22 16:25:41 +0800

    Updates

--- gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-import "./src/utils/global.scss"
-require("typeface-montserrat")
