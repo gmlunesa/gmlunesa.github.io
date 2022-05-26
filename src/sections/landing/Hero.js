18d5bab62292b2a58e2abdeb58aaa5f9fc950f8a
gmlunesa <gmlunesa@gmail.com>
2022-05-27 00:25:37 +0800

    Added PurgeCSS, fixed some issues found in production

--- src/sections/landing/Hero.js
+++ src/sections/landing/Hero.js
@@ -5,49 +5,11 @@ import imgHero from "../../assets/images/landing/hero-img.png"
 import imgHeroWave from "../../assets/images/landing/hero-wave.svg"
 import imgHeroWaveShadow from "../../assets/images/landing/hero-wave-shadow.svg"
 
-import imgShape1 from "../../assets/images/landing/hero-shape1.png"
-import imgShape2 from "../../assets/images/landing/hero-shape2.png"
-import imgShape3 from "../../assets/images/landing/hero-shape3.png"
-
 const Hero = () => {
   return (
     <>
       {/* <!-- Hero Area --> */}
-      <div
-        className="hero-landing pt-29 pt-lg-32 pb-5 position-relative z-index-1"
-        css={`
-          background-image: linear-gradient(
-            225deg,
-            #cc3e3e 0%,
-            #ff5f5f 39%,
-            #ff9494 100%
-          );
-        `}
-      >
-        <div
-          className="shape-1 gr-abs-tl z-index--1"
-          data-aos="fade-down-right"
-          data-aos-duration="600"
-          data-aos-delay="600"
-        >
-          <img src={imgShape1} alt="Shape as design" />
-        </div>
-        <div
-          className="shape-2 gr-abs-tl z-index--1"
-          data-aos="fade-down-right"
-          data-aos-duration="800"
-          data-aos-delay="800"
-        >
-          <img src={imgShape2} alt="Shape as design" />
-        </div>
-        <div
-          className="shape-3 gr-abs-tl z-index--1"
-          data-aos="fade-down-right"
-          data-aos-duration="1000"
-          data-aos-delay="1000"
-        >
-          <img src={imgShape3} alt="Shape as design" />
-        </div>
+      <div className="hero-landing pt-29 pt-lg-32 pb-5 position-relative z-index-1 bg-red">
         <Container>
           <Row className="justify-content-center">
             <Col md="9" lg="7" xl="6">
@@ -58,7 +20,7 @@ const Hero = () => {
                 <p className="gr-text-6 px-7 ps-md-0 pe-md-11 mb-0 gr-color-white-opacity-7">
                   Working as a{" "}
                   <span className="text-sky-blue">software engineer</span> with
-                  a considerable experience in building robust and complex
+                  considerable experience in building robust and complex
                   applications
                 </p>
                 <div
@@ -68,7 +30,7 @@ const Hero = () => {
                 >
                   <a
                     href="mailto:gmlunesa@gmail.com"
-                    className="btn btn-red rounded-8 gr-text-7 text-white gr-hover-y"
+                    className="btn btn-sky-blue rounded-8 gr-text-7 text-red gr-hover-y"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
