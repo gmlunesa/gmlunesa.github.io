e07c7fbf5fb345bffaec72fa85d917a47eba283d
gmlunesa <gmlunesa@gmail.com>
2023-05-04 13:09:27 +0800

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-f753abeda16232b8e49c.js"
+    "url": "webpack-runtime-c56be9c64e8d5c164988.js"
   },
   {
     "url": "framework-377d963b4077f80fba29.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "5aa0dab5ddb91c249a3fba7b175cbca9"
+    "revision": "23d116f9c50a332c02f547d25e2fbde8"
   },
   {
     "url": "polyfill-88799848bacff00fe1e3.js"
